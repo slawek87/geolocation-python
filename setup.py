6706195437de7dd7b58df242898b9539401d1b89
Alexandre Souza <alexandre_@fastmail.com>
2014-11-20 13:47:26 -0200

    added requirements

--- setup.py
+++ setup.py
@@ -21,4 +21,7 @@ setup(
                 'Application returns such information as: country, city, route/street, street number, lat and lng.',
     long_description=readme,
     keywords=['Google Maps Api', 'Google lat', 'Google lng', 'Google Maps'],
+    install_requires=[
+        'requests'
+    ],
 )
