dd5be769ff02ed3f916cdb15c4f1fb24344140ba
Sławomir Kabik <slawek@redsoftware.pl>
2014-11-25 21:31:19 +0100

    updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE.txt', 'r') as f:
 
 setup(
     name='geolocation-python',
-    version='0.1.2c',
+    version='0.1.2d',
     packages=['geolocation'],
     url='',
     download_url='https://github.com/slawek87/geolocation-python/',
